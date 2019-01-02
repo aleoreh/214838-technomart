0a68163b006f6ff5b99041b561a014687895ec41
aleoreh <aleoreh@gmail.com>
2019-01-02 18:57:05 +0300

    Decorate promo block elements

--- js/index.js
+++ js/index.js
@@ -9,6 +9,18 @@ var feedbackNameInput = feedbackModal.querySelector('[name=name]');
 var feedbackEmailInput = feedbackModal.querySelector('[name=email]');
 var feedbackTextarea = feedbackModal.querySelector('textarea');
 
+var currentSlide = 2;
+var sliderBackward = document.querySelector('.main-slider__rewind--backward');
+var sliderForward = document.querySelector('.main-slider__rewind--forward');
+var sliderItems = [
+    document.querySelector('.main-slider__slide--1'),
+    document.querySelector('.main-slider__slide--2'),
+];
+var sliderPosIndicators = [
+    document.querySelector('.main-slider__position-indicator--1'),
+    document.querySelector('.main-slider__position-indicator--2'),
+];
+
 try {
     storage = {
         name: localStorage.getItem('name'),
@@ -51,7 +63,7 @@ feedbackForm.addEventListener('submit', function(ev) {
     }
 });
 
-window.addEventListener('keydown', function (ev) {
+window.addEventListener('keydown', function(ev) {
     if (ev.keyCode === 27) {
         ev.preventDefault();
         if (feedbackModal.classList.contains('modal-show')) {
@@ -60,3 +72,36 @@ window.addEventListener('keydown', function (ev) {
     }
 });
 
+function switchSlider(cur) {
+    sliderItems.forEach(function(item) {
+        if (item.classList.contains('main-slider__slide--' + cur)) {
+            item.classList.add('main-slider__slide--current');
+        } else {
+            item.classList.remove('main-slider__slide--current');
+        }
+    });
+
+    sliderPosIndicators.forEach(function(item) {
+        if (item.classList.contains('main-slider__position-indicator--' + cur)) {
+            item.classList.add('main-slider__position-indicator--current');
+        } else {
+            item.classList.remove('main-slider__position-indicator--current');
+        }
+    });
+}
+
+sliderBackward.addEventListener('click', function(ev) {
+    ev.preventDefault();
+
+    currentSlide = currentSlide === 1 ? currentSlide : currentSlide - 1;
+    switchSlider(currentSlide);
+});
+
+sliderForward.addEventListener('click', function(ev) {
+    ev.preventDefault();
+
+    currentSlide = currentSlide === 2 ? currentSlide : currentSlide + 1;
+    switchSlider(currentSlide);
+});
+
+switchSlider(currentSlide);
